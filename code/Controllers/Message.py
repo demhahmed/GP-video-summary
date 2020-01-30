b2235fcc7f4a56b4cbfa9a82e56d4d3b3377d7d8
MoamenAttia <moamenattia@outlook.com>
2020-01-30 23:57:01 +0200

    Message Type Modified

--- code/Controllers/Message.py
+++ code/Controllers/Message.py
@@ -5,19 +5,19 @@ class Message:
     """ Message Controller to handle colored message in terminal to ease the job of debugging """
 
     @staticmethod
-    def info(message: int) -> None:
+    def info(message: str) -> None:
         """ Prints Blue Message """
         print(Fore.BLUE + message)
         print(Style.RESET_ALL)
 
     @staticmethod
-    def success(message: int) -> None:
+    def success(message: str) -> None:
         """ Prints Green Message """
         print(Fore.LIGHTGREEN_EX + message)
         print(Style.RESET_ALL)
 
     @staticmethod
-    def error(message: int) -> None:
+    def error(message: str) -> None:
         """ Prints Red Message """
         print(Fore.RED + message)
         print(Style.RESET_ALL)
