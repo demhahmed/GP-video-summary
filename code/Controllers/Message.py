9d2d48582efddf66ff77341131595367c3ac602b
MoamenAttia <moamenattia@outlook.com>
2020-01-31 03:45:44 +0200

    Scoreboard remove inheritance, add message progress

--- code/Controllers/Message.py
+++ code/Controllers/Message.py
@@ -2,22 +2,31 @@ from colorama import Fore, Style
 
 
 class Message:
-    """ Message Controller to handle colored message in terminal to ease the job of debugging """
-
+    """ Message Controller to handle
+        colored message in terminal to ease
+        the job of debugging.
+    """
     @staticmethod
     def info(message: str) -> None:
         """ Prints Blue Message """
         print(Fore.BLUE + message)
-        print(Style.RESET_ALL)
+        print(Style.RESET_ALL, end='')
 
     @staticmethod
     def success(message: str) -> None:
         """ Prints Green Message """
         print(Fore.LIGHTGREEN_EX + message)
-        print(Style.RESET_ALL)
+        print(Style.RESET_ALL, end='')
 
     @staticmethod
     def error(message: str) -> None:
         """ Prints Red Message """
         print(Fore.RED + message)
-        print(Style.RESET_ALL)
+        print(Style.RESET_ALL, end='')
+
+    @staticmethod
+    def progress(completed: int) -> None:
+        """ Prints progress bar """
+        print(Fore.GREEN + completed * "#", end='')
+        print(Fore.RED + (100 - completed) * "#")
+        print(Style.RESET_ALL, end='')
--- code/ScoreboardStrategy/Scoreboard.py
+++ code/ScoreboardStrategy/Scoreboard.py
@@ -1,19 +1,18 @@
 from pytesseract import image_to_string
 import cv2
-import numpy as np
 
-class Scoreboard:
+
+class PLScoreboard:
+    """ premier league scoreboard controller """
+
     def extract_results(self, image):
-        pass
+        """ extracts the results from premier league scoreboard """
 
-
-class PLScoreboard(Scoreboard):
-    def extract_results(self, image):
         # _, binary_image = cv2.threshold(image, 150, 255, cv2.THRESH_BINARY)
-        config = "--psm 7" #--oem 3 -c tessedit_char_whitelist=0123456789
+        config = "--psm 7"  # --oem 3 -c tessedit_char_whitelist=0123456789
 
         scale = 5
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
 
         # HOME TEAM
         temp = image[60*scale:80*scale, 165*scale:185*scale]
@@ -26,29 +25,40 @@ class PLScoreboard(Scoreboard):
         away_team_image = cv2.morphologyEx(temp, cv2.MORPH_CLOSE, kernel)
         cv2.imshow("away", away_team_image)
         away_team = image_to_string(away_team_image, lang="eng", config=config)
-
         return home_team, away_team
 
 
-class LaLigaScoreboard(Scoreboard):
+class LaLigaScoreboard:
+    """ LA_LIGA scoreboard controller """
+
     def extract_results(self, image):
+        """ extracts the results from LA_LIGA scoreboard """
         # to be implemented.
         pass
 
 
-class SeriaAScoreboar(Scoreboard):
+class SeriaAScoreboar:
+    """ SERIA_A scoreboard controller """
+
     def extract_results(self, image):
+        """ extracts the results from SERIA_A scoreboard """
         # to be implemented.
         pass
 
 
-class UCL2019(Scoreboard):
+class UCL2019:
+    """ CHAMPIONS_LEAGUE_2019 scoreboard controller """
+
     def extract_results(self, image):
+        """ extracts the results from CHAMPIONS_LEAGUE_2019 scoreboard """
         # to be implemented.
         pass
 
 
-class UCL2018(Scoreboard):
+class UCL2018:
+    """ CHAMPIONS_LEAGUE_2018 scoreboard controller """
+
     def extract_results(self, image):
+        """ extracts the results from CHAMPIONS_LEAGUE_2018 scoreboard """
         # to be implemented.
         pass
