da1980dd6f6704b3e73ec6e35153a108c065ac48
MoamenAttia <MoamenAttia@outlook.com>
2020-03-01 14:58:47 +0200

    new method for goal_detector

--- hight-level/goal_detector.py
+++ hight-level/goal_detector.py
@@ -1,64 +1,24 @@
 import cv2
-import pytesseract
-
-# Setup Environment for tesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+from skimage.measure import compare_ssim
 
 
 class GoalDetector:
-    def __init__(self):
-        self.__home = '0'
-        self.__away = '0'
-        self.__home_repeated = ''
-        self.__away_repeated = ''
+    def __init__(self, scoreboard_type='premier_league'):
+        if scoreboard_type == 'premier_league':
+            self.__height = [60, 80]
+            self.__width = [165, 215]
 
-    def __premier_league_scoreboard(self, image):
-        """ extracts the results from premier league scoreboard """
-        config = '--psm 7'
-        scale = 2
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-        temp = image[60 * scale:80 * scale, 165 * scale:185 * scale]
-        home_team_image = cv2.morphologyEx(temp, cv2.MORPH_CLOSE, kernel)
-        home_team = pytesseract.image_to_string(
-            home_team_image, lang='eng', config=config)
-        temp = image[60 * scale:80 * scale, 192 * scale:215 * scale]
-        away_team_image = cv2.morphologyEx(temp, cv2.MORPH_CLOSE, kernel)
-        away_team = pytesseract.image_to_string(
-            away_team_image, lang='eng', config=config)
-        return home_team, away_team
+    def execute(self, frame_1, frame_2):
+        frame_1 = cv2.cvtColor(frame_1, cv2.COLOR_BGR2GRAY)
+        frame_2 = cv2.cvtColor(frame_2, cv2.COLOR_BGR2GRAY)
 
-    def __extract_scoreboard_results(self, image):
-        [home, away] = self.__premier_league_scoreboard(image)
-        print(f'home:{home} away:{away}')
-        if home != '' and away != '':
-            if home.isnumeric() and away.isnumeric():
-                if home == self.__home and away == self.__away:
-                    self.__away_repeated = ''
-                    self.__home_repeated = ''
-                    return
-                if self.__home_repeated == home and self.__away_repeated == away:
-                    self.__home = home
-                    self.__away = away
-                    return True
-                self.__home_repeated = home
-                self.__away_repeated = away
+        # slicing
+        scoreboard_1 = frame_1[self.__height[0]:self.__height[1], self.__width[0]:self.__width[1]]
+        scoreboard_2 = frame_2[self.__height[0]:self.__height[1], self.__width[0]:self.__width[1]]
 
-    def execute(self, frames):
-        scale = 2
+        # check similarity index
+        score, _ = compare_ssim(scoreboard_1, scoreboard_2, full=True)
 
-        begin_frame = cv2.cvtColor(frames[0], cv2.COLOR_BGR2GRAY)
-        end_frame = cv2.cvtColor(frames[1], cv2.COLOR_BGR2GRAY)
-
-        begin_frame = cv2.resize(begin_frame, None, fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
-        end_frame = cv2.resize(end_frame, None, fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
-
-        _, begin_frame = cv2.threshold(begin_frame, 127, 255, cv2.THRESH_BINARY)
-        _, end_frame = cv2.threshold(begin_frame, 127, 255, cv2.THRESH_BINARY)
-
-        is_goal = self.__extract_scoreboard_results(begin_frame)
-        is_goal = self.__extract_scoreboard_results(end_frame)
-        if is_goal:
+        if score * 100 <= 90:
             print('Goal Detected')
-        return is_goal
-
-
+            return True
