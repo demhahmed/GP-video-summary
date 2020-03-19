bad96001e771605ae8c997face6bb16b019ca5fd
Ahmed Maher <demha.ahmed@yahoo.com>
2020-03-19 15:08:35 +0200

    goalpost manuel except hough

--- code/goalmouth/goalpost_manuel.py
+++ code/goalmouth/goalpost_manuel.py
@@ -1,6 +1,12 @@
-import cv2
 import numpy as np
+from canny import canny_main
+import cv2
 
+def image_to_gray(image):
+    """ Takes Image with channels b, g, r respectively """
+    # Y = 0.2125 R + 0.7154 G + 0.0721 B
+    gray_image = image.copy()
+    return np.array(0.2125 * gray_image[:, :, 2] + 0.7154 * gray_image[:, :, 1] + 0.0721 * gray_image[:, :, 0])
 
 def magnitude(x1, y1, x2, y2):
     return np.sqrt((x2-x1)**2.0 + (y2-y1)**2.0)
@@ -57,11 +63,11 @@ def goalMouth(frames):
         return False
 
 
-def goalpostv2(img):
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def goalpostv2(img_rgb):
+    img_bgr = img_rgb[..., ::-1]
+    gray = image_to_gray(img_bgr)
     #_, bw_img = cv2. threshold(gray, 127, 255, cv2.THRESH_BINARY)
-    edges = cv2.Canny(gray, 50, 200)
+    edges = canny_main(gray)
     #img_goalpost = cv2.medianBlur(edges, 5)
     kernel = np.ones((7, 7))
     edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
@@ -74,30 +80,3 @@ def goalpostv2(img):
         # print(False)
         return False
     return detect(lines)
-
-# pathnames = glob.glob("test-set/*.jpg")
-# k = 0
-# m = 0
-# for pathname in pathnames:
-#     img = cv2.imread(pathname, cv2.IMREAD_COLOR)
-#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#     #_, bw_img = cv2. threshold(gray, 127, 255, cv2.THRESH_BINARY)
-#     edges = cv2.Canny(gray, 50, 200)
-#     #img_goalpost = cv2.medianBlur(edges, 5)
-#     kernel = np.ones((7,7))
-#     edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
-#     kernel = np.ones((5,5), np.uint8)
-
-#     #edges = cv2.dilate(edges, kernel, iterations=1)
-#     cv2.imwrite('preprocess_'+str(m)+'.jpg', edges)
-#     m += 1
-#     lines = cv2.HoughLinesP(edges, 1, np.pi/180, 65, minLineLength=40, maxLineGap=10)
-#     if lines is None:
-#         print(pathname, False)
-#         continue
-#     a,b,c = lines.shape
-#     for i in range(a):
-#         cv2.line(edges, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (23, 32, 42), 3, cv2.LINE_AA)
-#     cv2.imwrite('houghlines_'+str(k)+'.jpg',edges)
-#     k += 1
-#     print(pathname, detect(lines))
