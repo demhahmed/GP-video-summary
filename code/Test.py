c7b4a2a8517c7f451de9a3d0ff59ea0b412e417e
ahmed1salama <asivo.ahmed@gmail.com>
2020-05-11 03:18:02 +0200

    two consecutive logos and appending last shot are done

---
+++ code/Test.py
@@ -0,0 +1,21 @@
+
+
+import cv2
+from ShotClassifier.ShotClassifier import ShotClassifier
+
+
+
+frames = []
+count = 1740
+while(1):
+    frames.append(cv2.imread(
+        "C:/Users\\salama\\Desktop\\GP-video-summary\\code\\frame%d.jpg" % count))
+    count += 5
+    if count == 1800:
+        break
+
+print( ShotClassifier(model_type=1).get_shot_class(
+                    frames))
+
+
+
--- code/main.py
+++ code/main.py
@@ -8,7 +8,7 @@ from UTL.UTL import *
 from ShotBoundary.ShotBoundary import cut_detector
 import bisect
 from moviepy.editor import VideoFileClip, concatenate
-from Audio.Audio import get_peak_times
+from Audio.audio import get_peak_times
 from ImageTools.ImageTools import ImageTools
 import cv2
 import numpy as np
@@ -16,26 +16,24 @@ import numpy as np
 
 def main():
     ############################## declarations #################################
-    VIDEO_PATH = 'C://Users\\medo\\Desktop\\test5.mp4'
+    VIDEO_PATH = 'C:/Users\\salama\\Desktop\\test3.mp4'
     cap = cv2.VideoCapture(VIDEO_PATH)
     if cap.isOpened() == False:
         print('err reading video')
     FPS = int(cap.get(cv2.CAP_PROP_FPS))
-
+    
     STEP = 5
-    frames, frame_times frame_numbers, shots, frames_to_classify, types = [], [], [], [], [], []
-    skip, patch, start = 0, 0, 0
+    frames, frame_times, frame_numbers, shots, frames_to_classify, types = [], [], [], [], [], []
+    skip, patch = 0, 0
     mouth, out = False, False
     type = ''                                                       # type of shot
     ############################## main loop ##################################
-    while 1:  # main loop
-        frames.clear()
-        frame_times.clear()
-        frame_numbers.clear()
+    while(1):  # main loop
         # append frames from after last cut
         count = 0
-
+        start = 0
         print("extracting patch ", patch, '\n')
+        p = patch*2000
         # extracting patch of 2000 frames
         while cap.isOpened():
             ret, image = cap.read()
@@ -46,7 +44,7 @@ def main():
             if ret == True:
                 if count % STEP == 0:
                     frames.append(image)
-                    frame_times.append(cap.get(cv2.CAP_PROP_POS_MSEC)/1000)
+                    frame_times.append(round(cap.get(cv2.CAP_PROP_POS_MSEC)/1000,1))
                     frame_numbers.append(count+patch*2000*5)
                 count += 1
             else:
@@ -63,12 +61,15 @@ def main():
 
             frame1, frame2 = frames[i], frames[i+1]
 
-            # detecting cut between the current 2 frames
-            if cut_detector(frame1, frame2) and abs(last_cut - frame_number) >= 20:
+            frame_number = frame_numbers[i]
+            frame_time = frame_times[i]
+            # detecting cut between the current 2 frame
+            if (cut_detector(frame1, frame2) and abs(last_cut - frame_number) >= 20):
 
-                no_shot_frames, skip = 0
+                no_shot_frames =0
+                skip = 0
                 mouth = False
-                frames_to_classify.clear()
+                frames_to_classify.clear()    
                 types.clear()
 
                 no_shot_frames = len(frames[start:i])
@@ -97,43 +98,55 @@ def main():
                     types = type.split("+")
                     if types[0] == "logo":
                         shots.append(
-                            (frame_number+(p*5), frame_time, False, "logo", False, False))
+                            (last_cut+25+(p*5), (last_cut+25)/FPS, False, "logo", False, False))
                         if types[1] not in ['logo', 'close-out', 'close']:
                             mouth = goalMouth(frames[i-20:i])
                         shots.append(
-                            (frame_number+5+(p*5), frame_time+5/FPS, False, types[1], mouth, False))
+                            (frame_number+(p*5), frame_time, False, types[1] , mouth, False))
 
                     else:
                         if types[0] not in ['logo', 'close-out', 'close']:
                             mouth = goalMouth(frames[i-25:i-5])
                         shots.append(
-                            (frame_number+(p*5), frame_time, False, types[0], mouth, False))
-                        shots.append((frame_number+5+(p*5), frame_time+(5/FPS),
+                            (frame_number-25+(p*5), frame_time-(25/FPS), False, types[0], mouth, False))
+                        shots.append((frame_number+(p*5), frame_time,
                                       False, "logo", False, False))
-                    del types
+                                      
                 last_cut = frame_number
                 start = i
         patch += 1
 
         if out:
             break
-
-    frames.clear()
-    del frames, frames_to_classify, skip, patch, mouth, out, type, no_shot_frames
+        frames = frames[-10:]
+        frame_times = frame_times[-10:]
+        frame_numbers = frame_numbers[-10:]
+        
+    #appending last shot in video 
+    shots.append((frame_numbers[-1],frame_times[-1],False, ShotClassifier(model_type=1).get_shot_class(frames),goalMouth(frames),False))
+    del frames_to_classify, skip, patch, mouth, out, type, no_shot_frames
+    ############################### resolving double logos ###########################
+    i = 0
+    while i <= len(shots)-2:
+        if shots[i][3] == "logo" and shots[i+1][3] == "logo":
+            shots.pop(i+1)
+            i-= 1
+        i+= 1
     ############################## audio processing ##################################
 
     print("Analyzing Audio...")
-    peak_times = get_peak_times(VIDEO_PATH)
+    peak_times = get_peak_times(VIDEO_PATH,90)
     print(peak_times)
 
     cuts = [x[1] for x in shots]
     final_times = []
     for peak in peak_times:
         index = find_gt(cuts, peak)
-        if cuts[index] < peak:
-            final_times.append((cuts[index], cuts[index+1]))
-        else:
-            final_times.append((cuts[index-1], cuts[index]))
+        if index != -1:
+            if cuts[index] < peak:
+                final_times.append((cuts[index], cuts[index+1]))
+            else:
+                final_times.append((cuts[index-1], cuts[index]))
 
     final_times = [t for t in (set(tuple(i) for i in final_times))]
 
@@ -149,29 +162,33 @@ def main():
                 shots[i] = (shots[i][0], shots[i][1], shots[i]
                             [2], shots[i][3], shots[i][4], True)
                 break
-
-    for shot in shots:
-        print(shot)
-
+    
+    f = open("output.txt","w")
+    for i in range(len(shots)):
+        f.write(str(shots[i][0])+" "+str(shots[i][1])+" "+str(shots[i][2])+" "+str(shots[i][3])+" "+str(shots[i][4])+" "+str(shots[i][5])+'\n')
+    f.close()
     ############################# processing output shots #################################
-    '''
+    
     output_video_shots = []
     logo_count = 0
     for i in range(len(shots)):
-        if shots[i][5] == "logo":
+        if shots[i][3] == "logo":
             logo_count+=1
 
         if logo_count == 2:
             j=i
-            while(1)
+            while(1):
                 output_video_shots.append(shots[j])
-                if logo_count == 0 and shots[j][5] == "wide":
+                if logo_count == 0 and shots[j][3] == "wide":
                     break
+                
+                if shots[j][3] == "logo":
+                    logo_count-=1
+
                 j-=1
-                logo_count-=1
 
     output_video_shots.reverse()
-    '''
+    print(output_video_shots)
     ################################## rendering video  ######################################
     '''
     print("rendering video...")
