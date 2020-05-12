e63234b4675d137bb8dfc7855c8416b434c75359
Moamen Hassan <moamenattia@outlook.com>
2020-05-12 16:35:22 +0200

    save state to localstorage to keep the redux presisted

--- gui/la5asly_client/src/index.js
+++ gui/la5asly_client/src/index.js
@@ -9,11 +9,36 @@ import { createStore, applyMiddleware, compose } from "redux";
 import reduxThunk from "redux-thunk";
 import reducers from "./reducers";
 
+const saveToLocalStorage = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const presistedState = loadFromLocalStorage();
+
 const store = createStore(
   reducers,
+  presistedState,
   composeEnhancers(applyMiddleware(reduxThunk))
 );
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 ReactDOM.render(
   <React.StrictMode>
