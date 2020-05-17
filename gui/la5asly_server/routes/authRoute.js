b3343637f89c033b6d99206211ac940da030d264
Moamen Hassan <moamenattia@outlook.com>
2020-05-18 00:46:10 +0200

    Auth Route for google OAuth2

---
+++ gui/la5asly_server/routes/authRoute.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const passport = require("passport");
+
+const router = new express.Router();
+
+/**
+ * This route handles the google authentication using passport oauth2.0
+ */
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+
+/**
+ * This route handles the google redirectaion from google oauth2.0
+ */
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google"),
+  (req, res) => {
+    res.redirect("/api/current_user");
+  }
+);
+
+
+/**
+ * This route extracts the user from the cookie and return it back to user.
+ */
+router.get("/api/current_user", (req, res) => {
+  res.send(req.user);
+});
+
+module.exports = router;
