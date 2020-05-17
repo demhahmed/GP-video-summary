0b965bc0aeef495db452fd93f5476c3130cd14dc
Moamen Hassan <moamenattia@outlook.com>
2020-05-18 00:12:31 +0200

    Secret keys for googleId and Cookies

---
+++ gui/la5asly_server/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientId:
+    "424362807497-f6tnei69eqf9qoam42cod4nail1gnq08.apps.googleusercontent.com",
+  googleClientSecret: "5c9td2waGyu_YMfEJYvREEiD",
+  cookieKey: "this is a secret hhhhh xD",
+};
