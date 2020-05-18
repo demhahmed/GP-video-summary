edaaa3129145a5a6ef6f2360996fd7861c32cf6a
Moamen Hassan <moamenattia@outlook.com>
2020-05-18 04:09:22 +0200

    add logout to server

--- gui/la5asly_server/routes/userRoute.js
+++ gui/la5asly_server/routes/userRoute.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const passport = require("passport");
+const auth = require("../middleware/auth");
 
 const router = new express.Router();
 
 /**
  * This route handles the google authentication using passport oauth2.0
  */
-router.post(
+router.get(
   "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
 );
 
 /**
@@ -18,25 +21,29 @@ router.get(
   "/auth/google/callback",
   passport.authenticate("google"),
   (req, res) => {
-    res.redirect("/api/current_user");
+    res.redirect("/");
   }
 );
 
 /**
  * This route handles the local authentication.
  */
-router.post(
-  "/auth/local",
-  passport.authenticate("local"),
-  (req, res) => {
-    res.redirect("/api/current_user");
-  }
-);
+router.post("/auth/local", passport.authenticate("local"), (req, res) => {
+  res.redirect("/");
+});
+
+/**
+ * This route logs out the user and clear the cookie.
+ */
+router.get("/api/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
 
 /**
  * This route extracts the user from the cookie and return it back to user.
  */
-router.get("/api/current_user", (req, res) => {
+router.get("/api/current_user", auth, (req, res) => {
   res.send(req.user);
 });
 
