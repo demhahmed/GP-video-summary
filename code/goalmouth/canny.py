60f0bd18a7cf043ae99471cf4c02519e45d52f18
Ahmed Maher <demha.ahmed@yahoo.com>
2020-08-17 06:10:33 +0200

    canny

--- code/goalmouth/canny.py
+++ code/goalmouth/canny.py
@@ -1,113 +1,81 @@
 import numpy as np
-from scipy.ndimage.filters import convolve
 from scipy import ndimage
+import cv2
+from scipy.ndimage.filters import convolve
 
 weak_pixel = 50
-strong_pixel = 200
-sigma = 1
-kernel_size = 5
+strong_pixel = 255
 
-def gaussian_kernel(size, sigma=1):
-    size = int(size) // 2
-    x, y = np.mgrid[-size:size+1, -size:size+1]
-    normal = 1 / (2.0 * np.pi * sigma**2)
-    g =  np.exp(-((x**2 + y**2) / (2.0*sigma**2))) * normal
-    return g
+def sobel_edge_detection(img):
+    sobel_filter_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
+    sobel_filer_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
 
-def sobel_filters(img):
-    Kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
-    Ky = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
+    I_x = ndimage.filters.convolve(img, sobel_filter_x)
+    I_y = ndimage.filters.convolve(img, sobel_filer_y)
 
-    Ix = ndimage.filters.convolve(img, Kx)
-    Iy = ndimage.filters.convolve(img, Ky)
-
-    G = np.hypot(Ix, Iy)
+    G = np.hypot(I_x, I_y)
     G = G / G.max() * 255
-    theta = np.arctan2(Iy, Ix)
-    return G, theta
+    direction = np.arctan2(I_y, I_x)
+    return G, direction
 
-def non_max_suppression(img, D):
-    M, N = img.shape
-    Z = np.zeros((M,N), dtype=np.int32)
-    angle = D * 180. / np.pi
-    angle[angle < 0] += 180
-
-    for i in range(1,M-1):
-        for j in range(1,N-1):
-            try:
-                q = 255
-                r = 255
-
-                if (0 <= angle[i,j] < 22.5) or (157.5 <= angle[i,j] <= 180):
-                    q = img[i, j+1]
-                    r = img[i, j-1]
-                elif (22.5 <= angle[i,j] < 67.5):
-                    q = img[i+1, j-1]
-                    r = img[i-1, j+1]
-                elif (67.5 <= angle[i,j] < 112.5):
-                    q = img[i+1, j]
-                    r = img[i-1, j]
-                elif (112.5 <= angle[i,j] < 157.5):
-                    q = img[i-1, j-1]
-                    r = img[i+1, j+1]
-
-                if (img[i,j] >= q) and (img[i,j] >= r):
-                    Z[i,j] = img[i,j]
-                else:
-                    Z[i,j] = 0
-
-            except IndexError as e:
-                pass
-
-    return Z
-
-def threshold(img):
-    lowThreshold = 0.05
-    highThreshold = 0.15
-
-    highThreshold = img.max() * highThreshold
-    lowThreshold = highThreshold * lowThreshold
-
-    M, N = img.shape
+def non_max_suppression(mag, direction):
+    M, N = mag.shape
     res = np.zeros((M,N), dtype=np.int32)
-
-    weak = np.int32(weak_pixel)
-    strong = np.int32(strong_pixel)
-
-    strong_i, strong_j = np.where(img >= highThreshold)
-    zeros_i, zeros_j = np.where(img < lowThreshold)
-
-    weak_i, weak_j = np.where((img <= highThreshold) & (img >= lowThreshold))
-
-    res[strong_i, strong_j] = strong
-    res[weak_i, weak_j] = weak
-
-    return res
-
-def hysteresis(img):
-
-    M, N = img.shape
-    weak = weak_pixel
-    strong = strong_pixel
+    angle = direction * 180. / np.pi
+    angle[angle < 0] += 180
 
     for i in range(1, M-1):
         for j in range(1, N-1):
-            if (img[i,j] == weak):
-                try:
-                    if ((img[i+1, j-1] == strong) or (img[i+1, j] == strong) or (img[i+1, j+1] == strong)
-                        or (img[i, j-1] == strong) or (img[i, j+1] == strong)
-                        or (img[i-1, j-1] == strong) or (img[i-1, j] == strong) or (img[i-1, j+1] == strong)):
-                        img[i, j] = strong
-                    else:
-                        img[i, j] = 0
-                except IndexError as e:
-                    pass
+
+            if (0 <= angle[i,j] < 22.5) or (157.5 <= angle[i,j] <= 180):
+                q = mag[i, j+1]
+                r = mag[i, j-1]
+            elif (22.5 <= angle[i,j] < 67.5):
+                q = mag[i+1, j-1]
+                r = mag[i-1, j+1]
+            elif (67.5 <= angle[i,j] < 112.5):
+                q = mag[i+1, j]
+                r = mag[i-1, j]
+            elif (112.5 <= angle[i,j] < 157.5):
+                q = mag[i-1, j-1]
+                r = mag[i+1, j+1]
+
+            if (mag[i,j] >= q) and (mag[i,j] >= r):
+                res[i,j] = mag[i,j]
+
+    return res
+
+def threshold(img, low= 50, high= 100):
+    res = np.zeros(img.shape)
+
+    strong_row, strong_col = np.where(img >= high)
+    weak_row, weak_col = np.where((img <= high) & (img >= low))
+
+    res[strong_row, strong_col] = strong_pixel
+    res[weak_row, weak_col] = weak_pixel
+    return res
+
+def edge_tracking(img):
+    for i in range(1, img.shape[0]-1):
+        for j in range(1, img.shape[1]-1):
+            if (img[i,j] == weak_pixel):
+                if ((img[i+1, j-1] == strong_pixel) 
+                    or (img[i+1, j] == strong_pixel) 
+                    or (img[i+1, j+1] == strong_pixel)
+                    or (img[i, j-1] == strong_pixel) 
+                    or (img[i, j+1] == strong_pixel)
+                    or (img[i-1, j-1] == strong_pixel) 
+                    or (img[i-1, j] == strong_pixel) 
+                    or (img[i-1, j+1] == strong_pixel)):
+                    img[i, j] = strong_pixel
+                else:
+                    img[i, j] = 0
 
     return img
 
-def canny_main(img):
-    img_smoothed = convolve(img, gaussian_kernel(kernel_size, sigma))
-    gradientMat, thetaMat = sobel_filters(img_smoothed)
-    nonMaxImg = non_max_suppression(gradientMat, thetaMat)
-    thresholdImg = threshold(nonMaxImg)
-    return hysteresis(thresholdImg)
+def canny_main(img, low, high):
+    img = cv2.GaussianBlur(img, (9, 9), 0)
+    grad_magnitude, grad_direction = sobel_edge_detection(img)
+    img = non_max_suppression(grad_magnitude, grad_direction)
+    img = threshold(img, low, high)
+    return edge_tracking(img)
